9caf02058bdca5b674944732d6bfc1f0fd2c865f
MudithaTharuka <mudithawyb234432@gmail.com>
2024-03-21 02:06:07 +0530

    Update the  themes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "rgba(var(--background))",
+        altBackground: "rgba(var(--altBackground))",
+        footerBackground: "rgba(var(--footerBackground))",
+        // border: "rgba(var(--border))",
+        // card: "rgba(var(--card))",
+        // "copy-primary": "rgba(var(--copy-primary))",
+        // "copy-secondary": "rgba(var(--copy-secondary))",
+        // cta: "rgba(var(--cta))",
+        // "cta-active": "rgba(var(--cta-active))",
+        // "cta-text": "rgba(var(--cta-text))",
+        // grape: "rgba(var(--grape))",--txt-primary: 242, 245, 247;
+        txtPrimary: "rgba(var(--txtPrimary))",
+        txtSecondary: "rgba(var(--txtSecondary))",
+        txtTertiary: "rgba(var(--txtTertiary))",
+        txtQuaternary: "rgba(var(--txtQuaternary))",
+      },
+      textColor: {},
+    },
   },
   plugins: [],
 };
