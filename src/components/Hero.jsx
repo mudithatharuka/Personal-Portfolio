d587dc066088cee2a965e462b4d2ee3da5faf76e
MudithaTharuka <mudithawyb234432@gmail.com>
2024-03-26 03:11:39 +0530

    Debug in hero bg animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,16 +10,19 @@ const Hero = () => {
       id="about"
       className={`${styles.paddingX} ${styles.paddingY} bg-background w-full mx-auto clip-path-[polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%)]`}
     >
-      <div className="flex justify-center relative max-w-7xl mx-auto z-1 ">
-        <div className="absolute flex justify-end right-0 bottom-0 overflow-hidden w-full h-full">
-          <HeroBgAnimation />
-        </div>
+      <div
+        id="left"
+        className="flex justify-center relative max-w-7xl mx-auto "
+      >
         <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-3 w-full max-w-7xl">
-          <div className="w-full">
+          <div className="absolute flex justify-end right-0 bottom-0 overflow-hidden w-full h-full">
+            <HeroBgAnimation />
+          </div>
+          <div className="w-full bg-slate-400 z-10">
             <p>Hi, I am</p>
             {/* <br /> */}
             <p>{profile.name}</p>
-            <span className="flex">
+            <span>
               I am a &nbsp;
               <span>
                 <Typewriter
@@ -31,28 +34,25 @@ const Hero = () => {
                 />
               </span>
             </span>
-            <p
-              onClick={() => {
-                console.log("ghghjgj");
-              }}
-            >
-              {profile.description}
-            </p>
-            <button
-              onClick={() => {
-                console.log("profile.urls.resume");
-                window.open(
-                  profile.urls.resume,
-                  "_blank"
-                  //   "noopener,noreferrer"
-                );
-              }}
-              className="bg-blue-300"
-            >
-              Check Resume
-            </button>
+            <p>{profile.description}</p>
+            <div className="z-50">
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  console.log("Button clicked!");
+                  window.open(
+                    profile.urls.resume,
+                    "_blank",
+                    "noopener,noreferrer"
+                  );
+                }}
+                className="hover:text-black cursor-pointer text-white bg-green-300"
+              >
+                Check Resume
+              </button>
+            </div>
           </div>
-          <div className="w-full bg-yellow-300"></div>
+          <div className="w-full bg-yellow-300 z-10"></div>
         </div>
       </div>
     </div>
