18cbc6eb1dfe0b6eba0ddac11c0339e004351fde
MudithaTharuka <mudithawyb234432@gmail.com>
2024-03-26 21:09:06 +0530

    Add the styles of the left hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ import HeroBgAnimation from "./HeroBgAnimation";
 import { styles } from "../styles";
 import { profile } from "../constants/data";
 import Typewriter from "typewriter-effect";
+import HeroImg from "../assets/New.png";
 
 const Hero = () => {
   return (
@@ -10,21 +11,20 @@ const Hero = () => {
       id="about"
       className={`${styles.paddingX} ${styles.paddingY} bg-background w-full mx-auto clip-path-[polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%)]`}
     >
-      <div
-        id="left"
-        className="flex justify-center relative max-w-7xl mx-auto "
-      >
-        <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-3 w-full max-w-7xl">
+      <div className="flex justify-center relative max-w-7xl mx-auto ">
+        <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-8 w-full max-w-7xl">
           <div className="absolute flex justify-end right-0 bottom-0 overflow-hidden w-full h-full">
             <HeroBgAnimation />
           </div>
-          <div className="w-full bg-slate-400 z-10">
-            <p>Hi, I am</p>
-            {/* <br /> */}
-            <p>{profile.name}</p>
-            <span>
+          <div className="w-full z-10 text-center md:text-left">
+            <p className="font-bold text-txtPrimary mb-2 text-[40px] lg:text-[50px] leading-[48px] lg:leading-[68px]">
+              Hi, I am
+              <br />
+              {profile.name}.
+            </p>
+            <span className="block md:flex font-semibold text-txtPrimary mb-4 text-[22px] lg:text-[32px] leading-[48px] lg:leading-[68px]">
               I am a &nbsp;
-              <span>
+              <span className="text-primary">
                 <Typewriter
                   options={{
                     strings: profile.roles,
@@ -34,25 +34,27 @@ const Hero = () => {
                 />
               </span>
             </span>
-            <p>{profile.description}</p>
+            <p className="text-txtSecondary text-[16px] lg:text-[20px] leading-[22px] lg:leading-[32px] mb-10">
+              {profile.description}
+            </p>
             <div className="z-50">
               <button
-                onClick={(e) => {
-                  e.stopPropagation();
-                  console.log("Button clicked!");
+                onClick={() => {
                   window.open(
                     profile.urls.resume,
                     "_blank",
                     "noopener,noreferrer"
                   );
                 }}
-                className="hover:text-black cursor-pointer text-white bg-green-300"
+                className={`${styles.padding} button-gradient btn-gradient w-[95%] max-w-[300px] text-center text-white rounded-3xl cursor-pointer font-semibold text-[18px] lg:text-[20px] transi`}
               >
                 Check Resume
               </button>
             </div>
           </div>
-          <div className="w-full bg-yellow-300 z-10"></div>
+          <div className="w-full bg-yellow-300 z-10">
+            <img src={HeroImg} alt="Hero Image" />
+          </div>
         </div>
       </div>
     </div>
