bee18f5bfc0500beca49f90985f10723d065b556
MudithaTharuka <mudithawyb234432@gmail.com>
2024-03-27 02:09:30 +0530

    Add the styles of the right hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,17 +12,17 @@ const Hero = () => {
       className={`${styles.paddingX} ${styles.paddingY} bg-background w-full mx-auto clip-path-[polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%)]`}
     >
       <div className="flex justify-center relative max-w-7xl mx-auto ">
-        <div className="flex flex-col-reverse md:flex-row items-center justify-between gap-8 w-full max-w-7xl">
-          <div className="absolute flex justify-end right-0 bottom-0 overflow-hidden w-full h-full">
+        <div className="flex flex-col-reverse md:flex-row items-center justify-between md:gap-8 w-full max-w-7xl">
+          <div className="absolute flex justify-end md:right-0  overflow-hidden w-full h-full max-w-[450px] md:max-w-[500px] lg:max-w-max">
             <HeroBgAnimation />
           </div>
-          <div className="w-full z-10 text-center md:text-left">
-            <p className="font-bold text-txtPrimary mb-2 text-[40px] lg:text-[50px] leading-[48px] lg:leading-[68px]">
+          <div className="w-full z-10 text-center md:text-left bg-background pt-4 pb-4">
+            <p className="font-bold text-txtPrimary mb-2 text-[30px] sm:text-[40px] lg:text-[50px] leading-[38px] sm:leading-[48px] lg:leading-[68px]">
               Hi, I am
               <br />
               {profile.name}.
             </p>
-            <span className="block md:flex font-semibold text-txtPrimary mb-4 text-[22px] lg:text-[32px] leading-[48px] lg:leading-[68px]">
+            <span className="block md:flex font-semibold text-txtPrimary mb-4 text-[22px] lg:text-[32px] lg:leading-[68px]">
               I am a &nbsp;
               <span className="text-primary">
                 <Typewriter
@@ -52,8 +52,12 @@ const Hero = () => {
               </button>
             </div>
           </div>
-          <div className="w-full bg-yellow-300 z-10">
-            <img src={HeroImg} alt="Hero Image" />
+          <div className="w-full flex mt-6 mb-6 justify-center z-10">
+            <img
+              src={HeroImg}
+              alt="Hero Image"
+              className="w-full h-full max-w-[280px] md:max-w-[400px] rounded-full border-[2px] border-primary"
+            />
           </div>
         </div>
       </div>
