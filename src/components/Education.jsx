9d089fdc7f85ada44ad6cd38373d82d47d840fb2
MudithaTharuka <mudithawyb234432@gmail.com>
2024-04-09 11:32:19 +0530

    Created the structure of the education section

---
+++ src/components/Education.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import SectionWrapper from "../hoc/SectionWrapper";
+
+const Education = () => {
+  return <div>Education</div>;
+};
+
+export default SectionWrapper(Education, "education");
