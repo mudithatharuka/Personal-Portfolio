1c262ff113f5e57038d44241f2966cd9c1733471
MudithaTharuka <mudithawyb234432@gmail.com>
2024-04-11 13:29:45 +0530

    Initiated the education card

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -3,6 +3,58 @@ import SectionWrapper from "../hoc/SectionWrapper";
 import { motion } from "framer-motion";
 import { textVariant } from "../utils/motion";
 import { styles } from "../styles";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import { education } from "../constants/data";
+
+const EducationCard = ({ education }) => {
+  return (
+    <VerticalTimelineElement
+      contentStyle={{
+        background: "transparent",
+        color: "rgba(var(--txtPrimary))",
+        border: "1px solid rgba(var(--primary))",
+        borderRadius: "10px",
+        boxShadow: "rgba(var(--boxShadow1)) 0px 4px 24px",
+      }}
+      contentArrowStyle={{ borderLeft: "10px solid rgba(var(--primary))" }}
+      iconStyle={{
+        background: "rgba(var(--cardBackground1))",
+        boxShadow:
+          "0 0 0 2px rgba(var(--primary)), inset 0 2px 0 rgba(0,0,0,.08), 0 3px 0 4px rgba(0,0,0,.05)",
+      }}
+      icon={
+        <div className="flex justify-center items-center w-full h-full">
+          <img
+            src={education?.img}
+            alt={education?.school}
+            className="w-5 h-[60%] object-contain"
+          />
+        </div>
+      }
+    >
+      <div>
+        <h3 className="font-semibold text-[14px] text-txtPrimary md:text-[18px]">
+          {education?.school}
+        </h3>
+        <h4 className="font-medium text-[12px] text-txtSecondary md:text-[14px]">
+          {education?.degree}
+        </h4>
+        {/* <h4 className="font-normal text-[11px] text-txtSecondary md:text-[13px]">
+          {education?.location}
+        </h4> */}
+        <h4
+          className={`font-normal text-[10px] text-txtSecondary md:text-[12px]`}
+        >
+          {education?.date}
+        </h4>
+      </div>
+      <p>{education.desc}</p>
+    </VerticalTimelineElement>
+  );
+};
 
 const Education = () => {
   return (
@@ -15,17 +67,15 @@ const Education = () => {
           </p>
         </motion.div>
 
-        {/* <div className={isOneCol ? "mr-[11%] md:ml-[7.5%] md:mr-[14.5%]" : ""}>
-      <VerticalTimeline>
-        {experiences?.map((experience, index) => (
-          <ExperienceCard
-            key={`experience-${index}`}
-            experience={experience}
-            isOneCol={isOneCol}
-          />
-        ))}
-      </VerticalTimeline>
-    </div> */}
+        {/* <div className="mr-[11%] md:ml-[7.5%] md:mr-[14.5%]"> */}
+        <div className="ml-[11%] md:mr-[7.5%] md:ml-[14.5%]">
+          {/* <div className="mx-[8%] md:mx-[14.5%]"> */}
+          <VerticalTimeline layout="1-column-right">
+            {education?.map((education, index) => (
+              <EducationCard key={`education-${index}`} education={education} />
+            ))}
+          </VerticalTimeline>
+        </div>
       </div>
     </div>
   );
