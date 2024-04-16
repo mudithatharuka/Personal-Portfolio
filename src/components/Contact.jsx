b7a218f7a0597037082b759a24f3df98be298766
MudithaTharuka <mudithawyb234432@gmail.com>
2024-04-16 12:31:54 +0530

    Added headers of contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
 import SectionWrapper from "../hoc/SectionWrapper";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className={`${styles.paddingX} ${styles.paddingY} w-full mx-auto`}>
+      <div className="max-w-7xl mx-auto flex flex-col-reverse md:flex-row md:gap-8 overflow-hidden">
+        <motion.div
+          variants={slideIn("left", "tween", 0.2, 1)}
+          className="pb-5"
+        >
+          <h2 className={`${styles.sectionHeadText}`}>Contact.</h2>
+          <p className={`${styles.sectionSubText}`}>Get in touch with me.</p>
+        </motion.div>
+      </div>
+    </div>
+  );
 };
 
 export default SectionWrapper(Contact, "contact");
