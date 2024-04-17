1760758530f997cc6c1e180b2f7df792fc570167
MudithaTharuka <mudithawyb234432@gmail.com>
2024-04-18 02:23:47 +0530

    Updated the styles in the contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,27 +1,30 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import SectionWrapper from "../hoc/SectionWrapper";
-import { slideIn } from "../utils/motion";
+import { slideIn, textVariant } from "../utils/motion";
 import EarthCanvas from "./canvas/Earth";
 
 const Contact = () => {
+  const formRef = useRef();
+  const [loading, setLoading] = useState(false);
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+
   return (
     <div className={`${styles.paddingX} ${styles.paddingY} w-full mx-auto`}>
-      <div className="max-w-7xl mx-auto flex flex-col-reverse md:flex-row md:gap-8 overflow-hidden">
-        <motion.div
-          variants={slideIn("left", "tween", 0.2, 1)}
-          className="pb-5"
-        >
+      <div className="max-w-7xl mx-auto">
+        <motion.div variants={textVariant()} className="pb-5">
           <h2 className={`${styles.sectionHeadText}`}>Contact.</h2>
           <p className={`${styles.sectionSubText}`}>Get in touch with me.</p>
         </motion.div>
-        <motion.div
-          variants={slideIn("right", "tween", 0.2, 1)}
-          className="pb-5"
-        >
-          <EarthCanvas />
-        </motion.div>
+        <div className="flex flex-col-reverse md:flex-row md:gap-8 overflow-hidden justify-center my-[30px]">
+          <motion.div
+            variants={slideIn("right", "tween", 0.2, 1)}
+            className="pb-5"
+          >
+            <EarthCanvas />
+          </motion.div>
+        </div>
       </div>
     </div>
   );
